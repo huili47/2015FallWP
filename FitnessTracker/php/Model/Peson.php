b48b1cca5ffac4059eaccf62fdbc1ee3eff63331
Hui Li <lih1@hawkmail.newpaltz.edu>
2015-12-06 03:20:42 +0000

    ajax

--- FitnessTracker/php/Model/Peson.php
+++ FitnessTracker/php/Model/Peson.php
@@ -1,9 +1,33 @@
 <?php
-    include '../inc/global.php';
-    class Person {
-        static function Get(){
-            return FetchAll("SELECT * FROM 2015Fall_Persons");
-        }
+require_once '../inc/global.php';
+class Person {
+    public static function Get($id = null){
+        $sql = "SELECT * FROM 2015Fall_Persons";
+        
+		if($id){
+			$sql .= " WHERE id=$id ";
+			$ret = FetchAll($sql);
+			return $ret[0];
+		}else{
+			return FetchAll($sql);			
+		}
+		
     }
     
-    
+    static public function Delete($id)
+	{
+		$conn = GetConnection();
+		$sql = "DELETE FROM 2015Fall_Persons WHERE id = $id";
+		//echo $sql;
+		$results = $conn->query($sql);
+		$error = $conn->error;
+		$conn->close();
+		
+		return $error ? array ('sql error' => $error) : false;
+	}
+	
+	static public function Blank()
+	{
+		return array();
+	}
+}
