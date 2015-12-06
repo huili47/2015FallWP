173522edd894a817a69d0725d915ac93c6bba208
Hui Li <lih1@hawkmail.newpaltz.edu>
2015-12-06 03:30:20 +0000

    updates

--- FitnessTracker/Views/persons/edit.php
+++ FitnessTracker/Views/persons/edit.php
@@ -1,5 +1,13 @@
-<form>
+<form method="post" action="?action=save">
+ <?php include __DIR__ . '/../shared/_Errors.php'; ?>
+ 
+  <table class="table">
     <input type="text" name="Name" class="form-control" placeholder="Name" value="<?=$model['Name']?>" />
     <input type="text" name="Birthday" class="form-control" placeholder="Birthday" value="<?=$model['Birthday']?>" />
     <input type="submit" value="Submit" class="btn btn-primary"/>
+       <td>
+         <input type="submit" value="Submit" class="btn btn-primary"/>
+         <input type="hidden" name="id" value="<?=$model['id']?>" /> 
+       </td>
+     </table> 
 </form>
